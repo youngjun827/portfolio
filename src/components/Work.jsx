755ce517dc82dd5c1c21d078c73b3f8129e2ef7f
youngjun827 <youngjunjoo827@gmail.com>
2023-08-14 17:14:53 -0400

    chore: Cleaned up legacy projects, paving the way for the exciting new addition 🚀

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,93 +3,23 @@ import React from "react";
 import WorkImg from "../assets/comming-soon.webp";
 import UW from "../assets/uw-screen-shot.png";
 import Smile from "../assets/Smile.png";
-import MLH from "../assets/mlh.jpeg"
-
+import MLH from "../assets/mlh.jpeg";
 
 const Work = () => {
   return (
-    <div name="work" className="w-full md:h-full bg-[#3e3e1f] py-[3rem]">
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full ">
-        <div className="pb-8">
-          <p className="text-[#ede7dd] text-4xl font-bold inline border-b-4 border-[#8a3918] border-600 ">
+    <div name='work' className='w-full md:h-full bg-[#3e3e1f] py-[3rem]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full '>
+        <div className='pb-8'>
+          <p className='text-[#ede7dd] text-4xl font-bold inline border-b-4 border-[#8a3918] border-600 '>
             Work
           </p>
-          <p className="py-4 text-[#ede7dd]">
+          <p className='py-4 text-[#ede7dd]'>
             // Check out some of my recent work
           </p>
         </div>
 
         {/* Container */}
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-
-          {/* Grid Item */}
-          <div
-            style={{
-              backgroundImage: `url(${MLH})`,
-              backgroundSize: "contain",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-            }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div bg-[#FAFAFA]"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-black tracking-wider">
-                MLH PREP PROJECT 
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://mlh-prep-22-nov-prep-2-project.netlify.app/"
-                target="_blank" rel="noreferrer">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="https://github.com/MLH-Fellowship/prep-project-22.NOV.PREP.2"
-                target="_blank" rel="noreferrer">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-
-          {/* Grid Item */}
-          <div
-            style={{
-              backgroundImage: `url(${Smile})`,
-              backgroundSize: "contain",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
-            }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div bg-[#1B1B1B]"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-black tracking-wider">
-                Video Chat App
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  href="https://youngjun-video-chat-app.netlify.app"
-                  target="_blank" rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  href="https://github.com/youngjun827/video-chat-app"
-                  target="_blank" rel="noreferrer"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
           {/* Grid Item */}
           <div
             style={{
@@ -98,27 +28,29 @@ const Work = () => {
               backgroundPosition: "center",
               backgroundRepeat: "no-repeat",
             }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div bg-[#FFEE00]"
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div bg-[#FFEE00]'
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-black tracking-wider">
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-black tracking-wider'>
                 UW GitHub Finder
               </span>
-              <div className="pt-8 text-center">
-                <a 
-                href="https://uwaterloo-github-finder.netlify.app/"    
-                target="_blank" rel="noreferrer"
+              <div className='pt-8 text-center'>
+                <a
+                  href='https://uwaterloo-github-finder.netlify.app/'
+                  target='_blank'
+                  rel='noreferrer'
                 >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a 
-                href="https://github.com/youngjun827/uwaterloo-github-finder"
-                target="_blank" rel="noreferrer"
+                <a
+                  href='https://github.com/youngjun827/uwaterloo-github-finder'
+                  target='_blank'
+                  rel='noreferrer'
                 >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
                 </a>
